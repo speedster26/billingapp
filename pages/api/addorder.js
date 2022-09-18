86cb8ac8c7466cea75bb4b8727505bb66b41e8c3
Harshit Verma <harshitverma591@gmail.com>
2022-09-18 23:01:53 +0530

    Bugs fix 1.4

--- pages/api/addorder.js
+++ pages/api/addorder.js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Order from "../../models/Order";
 import Products from "../../models/Products";
 import connectDb from "../../middleware/mongoose"
 
@@ -6,21 +7,24 @@ const handler = async (req, res) => {
 
     try {
         if (req.method == 'POST') {
-            for (let i = 0; i < req.body.length; i++) {
-                let p = new Products({
-                    title: req.body[i].title,
-                    price: req.body[i].price,
-                    desc: req.body[i].desc,
-                    image: req.body[i].image,
-                    serNo: req.body[i].serNo,
-                    category: req.body[i].category,
-                    color: req.body[i].color,
-                    size: req.body[i].size,
-                    availableQty: req.body[i].availableQty
-                })
-                await p.save();
+            const { orderId, custInfo, custAddress, orderItems, orderTotal, orderStatus, operatorId, paymentMethod} = req.body.order
+            let order = new Order({
+                orderId,
+                custInfo,
+                custAddress,
+                orderItems,
+                orderTotal,
+                orderStatus,
+                operatorId,
+                paymentMethod
+            })
+            await order.save()
+            for (let item of orderItems) {
+                let pro = await Products.findById(item._id);
+                pro.availableQty = pro.availableQty - item.qty
+                await Products.findByIdAndUpdate(item._id, pro)
             }
-            res.status(200).json({ success: true})
+            res.status(200).json({ success: true , orderId})
         }
         else {
             res.status(400).json({ success: false, error: "This method is not allowed" })
